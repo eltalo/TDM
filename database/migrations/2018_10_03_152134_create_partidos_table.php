de080705d4dc855c8fae83d4cefe7a68c891eb7b
unknown <eltalo@gmail.com>
2018-10-04 09:08:35 -0300

    first commit

---
+++ database/migrations/2018_10_03_152134_create_partidos_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePartidosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Partidos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedTinyInteger('IDJugador1');
+            $table->unsignedTinyInteger('IDJugador2');
+            $table->unsignedTinyInteger('PtosJugador1Set1');
+            $table->unsignedTinyInteger('PtosJugador2Set1');
+            $table->unsignedTinyInteger('PtosJugador1Set2');
+            $table->unsignedTinyInteger('PtosJugador2Set2');
+            $table->unsignedTinyInteger('PtosJugador1Set3');
+            $table->unsignedTinyInteger('PtosJugador2Set3');
+            $table->unsignedTinyInteger('PtosJugador1Set4');
+            $table->unsignedTinyInteger('PtosJugador2Set4');
+            $table->unsignedTinyInteger('PtosJugador1Set5');
+            $table->unsignedTinyInteger('PtosJugador2Set5');
+            $table->unsignedTinyInteger('PtosJugador1Set6');
+            $table->unsignedTinyInteger('PtosJugador2Set6');
+            $table->unsignedTinyInteger('PtosJugador1Set7');
+            $table->unsignedTinyInteger('PtosJugador2Set7');
+            $table->dateTime('FechaPartido');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Partidos');
+    }
+}
